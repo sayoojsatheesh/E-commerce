9985dcaad420a7db7f6f9ef2563cc33547c3e509
unknown <sayooj0076@gmail.com>
2023-10-03 18:37:32 +0530

    6:37

--- Backend/controllers/products-controllers.js
+++ Backend/controllers/products-controllers.js
@@ -1,14 +1,30 @@
 const HttpError = require("../models/http-error");
 const mongoose = require("mongoose");
-const productList=require('../models/products')
+const productList = require("../models/products");
 
 const getAllItems = async (req, res, next) => {
-  let products;
+  let offset = +req.query.offset || 0;
+  const limit = 9; // Number of items per page
+
   try {
-    products = await productList.find();
-    console.log(products);
+    const products = await productList.find().skip(offset).limit(limit).select({
+      id: 1,
+      title: 1,
+      subTitle: 1,
+      price: 1,
+      description: 1,
+      colour: 1,
+      style: 1,
+      review: 1,
+      image1: 1,
+    });
+
+    const totalCount = await productList.countDocuments({});
+    offset = offset + 6;
+
+    res.status(200).json({ products, totalCount, offset });
   } catch (err) {
-    console.log(err)
+    console.error(err);
     const error = new HttpError(
       "Something went wrong, could not find places.",
       500
