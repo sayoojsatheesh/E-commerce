3b184f85604b6435c6fce2715389e0fce74c7181
unknown <sayooj0076@gmail.com>
2023-10-04 18:27:56 +0530

    6:27

--- Backend/controllers/products-controllers.js
+++ Backend/controllers/products-controllers.js
@@ -4,23 +4,52 @@ const productList = require("../models/products");
 
 const getAllItems = async (req, res, next) => {
   let offset = +req.query.offset || 0;
+  let filters = req.query.filters;
+  let sortBy = filters?.sortBy;
   const limit = 9; // Number of items per page
 
   try {
-    const products = await productList.find().skip(offset).limit(limit).select({
-      id: 1,
-      title: 1,
-      subTitle: 1,
-      price: 1,
-      description: 1,
-      colour: 1,
-      style: 1,
-      review: 1,
-      image1: 1,
-    });
+    let order;
+    if (!sortBy) {
+      order = 0;
+    } else {
+      order = sortBy == "Price: High-Low" ? -1 : 1;
+    }
+
+    let products;
+    if (sortBy) {
+      products = await productList
+        .find()
+        .skip(offset)
+        .limit(limit)
+        .select({
+          id: 1,
+          title: 1,
+          subTitle: 1,
+          price: 1,
+          description: 1,
+          colour: 1,
+          style: 1,
+          review: 1,
+          image1: 1,
+        })
+        .sort({ price: order });
+    } else {
+      products = await productList.find().skip(offset).limit(limit).select({
+        id: 1,
+        title: 1,
+        subTitle: 1,
+        price: 1,
+        description: 1,
+        colour: 1,
+        style: 1,
+        review: 1,
+        image1: 1,
+      });
+    }
 
     const totalCount = await productList.countDocuments({});
-    offset = offset + 6;
+    offset = offset + 9;
 
     res.status(200).json({ products, totalCount, offset });
   } catch (err) {
