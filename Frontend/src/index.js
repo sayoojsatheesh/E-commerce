c5ccc1cb9d2b7260efd30cb686b0973709cdc5e7
unknown <sayooj0076@gmail.com>
2023-10-26 18:28:15 +0530

    6:32

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,21 +1,29 @@
+// React //
 import React from "react";
+// React Dom //
 import ReactDOM from "react-dom/client";
+// Redux //
+import { Provider } from "react-redux";
+// Other //
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { store } from "./apps/store";
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </QueryClientProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
