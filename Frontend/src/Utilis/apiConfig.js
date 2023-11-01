edd3111c5eab61cc5f6d7019aceff9232930b3c0
unknown <sayooj0076@gmail.com>
2023-11-01 16:51:35 +0530

    4:51

--- Frontend/src/Utilis/apiConfig.js
+++ Frontend/src/Utilis/apiConfig.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api'; // Replace with your API base URL
+const API_BASE_URL = "https://ecommerce-shoes-data2.onrender.com/api"; // Replace with your API base URL
+// const API_BASE_URL = 'http://localhost:5000/api';
 
 export default API_BASE_URL;
