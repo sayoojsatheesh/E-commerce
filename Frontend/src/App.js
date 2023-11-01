94573265e7d3ce8f26dbbccae2a73970aed9f7d2
unknown <sayooj0076@gmail.com>
2023-11-01 12:02:18 +0530

    12:02

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 // React //
-import { useState } from "react";
+import React, { useState, Suspense } from "react";
 // Other //
 import { Routes, Route } from "react-router-dom";
 // CSS //
@@ -8,36 +8,52 @@ import "./App.css";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Footer from "./Components/Shared/Footer/Footer";
-import DisplayProducts from "./Pages/DisplayProducts/DisplayProducts";
-import SingleProduct from "./Pages/SingleProduct/SingleProduct";
-import CartPage from "./Components/CartPage/CartPage";
 import ScrollToTop from "./Components/Shared/ScrollToTop/ScrollToTop";
-import SearchPage from "./Components/SearchPage/SearchPage";
+// import DisplayProducts from "./Pages/DisplayProducts/DisplayProducts";
+// import SingleProduct from "./Pages/SingleProduct/SingleProduct";
+// import CartPage from "./Components/CartPage/CartPage";
+// import SearchPage from "./Components/SearchPage/SearchPage";
+
+const DisplayProducts = React.lazy(() =>
+  import("./Pages/DisplayProducts/DisplayProducts")
+);
+const SingleProduct = React.lazy(() =>
+  import("./Pages/SingleProduct/SingleProduct")
+);
+const CartPage = React.lazy(() => import("./Components/CartPage/CartPage"));
+const SearchPage = React.lazy(() =>
+  import("./Components/SearchPage/SearchPage")
+);
 
 function App() {
   const [showSearchPage, setshowSearchPage] = useState(false);
 
   return (
-    <div>
-      <Navbar
-        showSearchPage={showSearchPage}
-        setshowSearchPage={setshowSearchPage}
-      />
-      <ScrollToTop />
-      <SearchPage showSearchPage={showSearchPage}  setshowSearchPage={setshowSearchPage}/>
-      <Routes>
-        <Route path="/products/all" element={<DisplayProducts />} />
-        <Route path="/products/men" element={<DisplayProducts />} />
-        <Route path="/products/women" element={<DisplayProducts />} />
-        <Route path="/products/kids" element={<DisplayProducts />} />
-        <Route path="/products/cart" element={<CartPage />} />
-        <Route path="/products/favourites" element={<CartPage />} />
-        <Route path="/products/searchpage" element={<SearchPage />} />
-        <Route path="/products/:productId" element={<SingleProduct />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Suspense >
+      <div>
+        <Navbar
+          showSearchPage={showSearchPage}
+          setshowSearchPage={setshowSearchPage}
+        />
+        <ScrollToTop />
+        <SearchPage
+          showSearchPage={showSearchPage}
+          setshowSearchPage={setshowSearchPage}
+        />
+        <Routes>
+          <Route path="/products/all" element={<DisplayProducts />} />
+          <Route path="/products/men" element={<DisplayProducts />} />
+          <Route path="/products/women" element={<DisplayProducts />} />
+          <Route path="/products/kids" element={<DisplayProducts />} />
+          <Route path="/products/cart" element={<CartPage />} />
+          <Route path="/products/favourites" element={<CartPage />} />
+          <Route path="/products/searchpage" element={<SearchPage />} />
+          <Route path="/products/:productId" element={<SingleProduct />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
 
